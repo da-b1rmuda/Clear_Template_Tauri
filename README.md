29d44845598c334afea5ea9055b3f920c71d8a2b
da_b1rmuda <lamurka1337@yandex.ru>
2024-05-07 17:29:00 +0400

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tauri + React + Typescript
+
+# + Updater
+
+## Change
+
+package.json:
+
+- name
+
+tauri.config.json:
+
+- productName
+- identifier
+- publisher
+- copyright
+- icon
+- endpoints
+- pubkey
+- title
+
+cargo.toml:
+
+- name
+- description
+- author
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: ["es2021", "chrome100", "safari13"],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+    outDir: "build",
+  },
+});
